4ab150688fe95a152ab0ac3c9c52c6c55b1bfb70
HUGH-ALI <2892215029@qq.com>
2026-08-19 06:50:23 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-function showMessage() {
-    alert("欢迎来到我的个人网站！");
-}
-
 function goToProjects() {
+
     document.getElementById("projects").scrollIntoView({
         behavior: "smooth"
     });
+
 }
