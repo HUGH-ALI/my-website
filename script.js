f495c888d29420a1d36dad5b863f2368274c167b
HUGH-ALI <2892215029@qq.com>
2026-08-19 22:37:18 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-function goToProjects() {
+function login() {
 
-    document.getElementById("projects").scrollIntoView({
-        behavior: "smooth"
-    });
+    const password = document.getElementById("password").value;
 
+    const correctPassword = "123456";
+
+    if (password === correctPassword) {
+
+        sessionStorage.setItem("loggedIn", "true");
+
+        window.location.href = "index.html";
+
+    } else {
+
+        document.getElementById("message").textContent =
+            "𝑪𝒊𝒂𝒍𝒍𝒐～(∠・ω< )⌒☆，密码是你生日哦";
+    }
 }
